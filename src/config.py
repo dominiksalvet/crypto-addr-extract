a7e7b67ac055bf5306890a8792d54dce1478e065
Dominik Salvet <dominik.salvet@gmail.com>
2022-04-28 16:04:47 +0200

    use big dataset un default

--- src/config.py
+++ src/config.py
@@ -6,4 +6,4 @@ ACCEPTED_EXTS_PATH = "accepted_exts" # accepted file extensions
 REFUSED_EXTS_PATH = "refused_exts" # refused file extensions
 # when both accepted and refused have no match, it accepts 'text/*' MIME type
 EXTRA_MIMES_PATH = "extra_mimes" # also accept MIME types listed in this file
-DATASET_DIR = "small_dataset" # the root path of the uncompressed dataset
+DATASET_DIR = "dataset" # the root path of the uncompressed dataset
