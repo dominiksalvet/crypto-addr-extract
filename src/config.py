012d076c3b6c825ad905ffdfa731a3ce5dc4f91a
Dominik Salvet <dominik.salvet@gmail.com>
2022-04-28 14:54:10 +0200

    add first template

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# author: Dominik Salvet
+# login: xsalve03
+
+# file path constants
+ACCEPTED_EXTS_PATH = "accepted_exts" # accepted file extensions
+REFUSED_EXTS_PATH = "refused_exts" # refused file extensions
+# when both accepted and refused have no match, it accepts 'text/*' MIME type
+EXTRA_MIMES_PATH = "extra_mimes" # also accept MIME types listed in this file
+DATASET_DIR = "small_dataset" # the root path of the uncompressed dataset
